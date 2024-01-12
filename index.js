eec738d2be54920acbe5106adad30587d61b077a
AliKales <ali.kales@hotmail..com>
2024-01-12 21:18:47 +0300

    navigator

---
+++ index.js
@@ -0,0 +1,19 @@
+function aboutMe() {
+    document.getElementById("aboutMeTitle").scrollIntoView();
+}
+
+function mySkills() {
+    document.getElementById("mySkillsTitle").scrollIntoView();
+}
+
+function myWorks() {
+    document.getElementById("myWorksTitle").scrollIntoView();
+}
+
+function mySocial() {
+    document.getElementById("mySocialTitle").scrollIntoView();
+}
+
+function sendEmail() {
+    window.open('mailto:ali.kales@hotmail.com');
+}
